7b88b962788f9fc6ebb961e77f5724bcab8ad834
TONI7008 <tgnmhtt@gmail.com>
2025-09-23 15:10:07 -0400

    Remove build directory from tracking

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TWidget_FILE C:/Users/Toni/Documents/cpp/TWidget/build/TWidget.exe)
-set(__QT_DEPLOY_TARGET_TWidget_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TWidget_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
