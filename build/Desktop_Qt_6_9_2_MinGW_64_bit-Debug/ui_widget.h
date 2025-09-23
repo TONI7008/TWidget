f517802ff2f0c1acf83d789cb74031802255254e
TONI7008 <tgnmhtt@gmail.com>
2025-09-23 15:02:43 -0400

    moving to cmake

---
+++ build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/ui_widget.h
@@ -0,0 +1,76 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 6.9.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QVBoxLayout *verticalLayout;
+    QLabel *label;
+    QSpacerItem *verticalSpacer;
+    QLabel *statutLabel;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName("Widget");
+        Widget->resize(800, 600);
+        verticalLayout = new QVBoxLayout(Widget);
+        verticalLayout->setObjectName("verticalLayout");
+        label = new QLabel(Widget);
+        label->setObjectName("label");
+        label->setStyleSheet(QString::fromUtf8("font: 700 14pt \"Noto Sans\";"));
+        label->setAlignment(Qt::AlignmentFlag::AlignCenter);
+
+        verticalLayout->addWidget(label);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Expanding);
+
+        verticalLayout->addItem(verticalSpacer);
+
+        statutLabel = new QLabel(Widget);
+        statutLabel->setObjectName("statutLabel");
+        statutLabel->setMinimumSize(QSize(0, 100));
+        statutLabel->setStyleSheet(QString::fromUtf8("font: 700 14pt \"Noto Sans\";"));
+        statutLabel->setAlignment(Qt::AlignmentFlag::AlignCenter);
+
+        verticalLayout->addWidget(statutLabel);
+
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
+        label->setText(QCoreApplication::translate("Widget", "Do something", nullptr));
+        statutLabel->setText(QCoreApplication::translate("Widget", "TextLabel", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
